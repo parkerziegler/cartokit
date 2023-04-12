e2ac011d5aacff5a0191425e9baa0c03b1b31315
parkerziegler <parkerellisziegler@gmail.com>
2023-04-12 10:13:35 -0700

    feat: add placeholder loading spinner for data uploads

--- src/lib/components/layers/FromAPI.svelte
+++ src/lib/components/layers/FromAPI.svelte
@@ -12,11 +12,13 @@
   import { map } from '$lib/stores/map';
   import type { CartoKitFillLayer } from '$lib/types/CartoKitLayer';
   import { randomColor } from '$lib/utils/color';
+  import type { MapSourceDataEvent } from 'maplibre-gl';
 
   const closeModal = getContext<() => void>('close-modal');
 
   let endpoint = '';
   let displayName = '';
+  let dataLoading = false;
 
   function onEndpointChange(event: CustomEvent<{ value: string }>) {
     endpoint = event.detail.value;
@@ -26,7 +28,19 @@
     displayName = event.detail.value;
   }
 
+  function onDataLoaded(layer: CartoKitFillLayer) {
+    return function handleDataLoaded(event: MapSourceDataEvent) {
+      if (event.sourceId === layer.id) {
+        dataLoading = false;
+        closeModal();
+        $map.off('sourcedata', onDataLoaded);
+      }
+    };
+  }
+
   function onSubmit() {
+    dataLoading = true;
+
     const layer: CartoKitFillLayer = {
       id: uniqueId(kebabCase(displayName)),
       displayName,
@@ -44,15 +58,12 @@
 
     layers.update((lyrs) => {
       lyrs[layer.id] = layer;
-
-      if ($map) {
-        addSource($map, layer);
-      }
+      addSource($map, layer);
 
       return lyrs;
     });
 
-    closeModal();
+    $map.on('sourcedata', onDataLoaded(layer));
   }
 </script>
 
@@ -77,5 +88,35 @@
       class="w-full"
     />
   </div>
-  <Button class="self-end">Add</Button>
+  <Button class="self-end">
+    {#if dataLoading}
+      <span class="loader" />
+    {:else}
+      Add
+    {/if}
+  </Button>
 </form>
+
+<style>
+  .loader {
+    @apply relative inline-block h-4 w-4;
+  }
+  .loader::after,
+  .loader::before {
+    @apply absolute left-0 top-0 h-4 w-4 rounded-full bg-white;
+    content: '';
+    animation: animloader 0.5s ease-in-out infinite;
+  }
+
+  @keyframes animloader {
+    0% {
+      transform: scale(0);
+      opacity: 1;
+    }
+
+    100% {
+      transform: scale(1);
+      opacity: 0;
+    }
+  }
+</style>
