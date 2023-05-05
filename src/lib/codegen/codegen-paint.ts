d0bd51c763b6b615da96319aeb75268112da8038
parkerziegler <parkerellisziegler@gmail.com>
2023-05-05 09:48:25 -0700

    fix: remove unnecessary intersperse function

--- src/lib/codegen/codegen-paint.ts
+++ src/lib/codegen/codegen-paint.ts
@@ -13,74 +13,70 @@ import { MAPBOX_DEFAULTS } from '$lib/utils/constants';
 export function codegenFill(layer: CartoKitLayer): string {
   switch (layer.type) {
     case 'Fill': {
-      return intersperse(
-        [
-          withDefault(
-            'fill-color',
-            layer.style.fill,
-            MAPBOX_DEFAULTS['fill-color']
-          ),
-          withDefault(
-            'fill-opacity',
-            layer.style.opacity,
-            MAPBOX_DEFAULTS['fill-opacity']
-          )
-        ],
-        ',\n'
-      );
+      return [
+        withDefault(
+          'fill-color',
+          layer.style.fill,
+          MAPBOX_DEFAULTS['fill-color']
+        ),
+        withDefault(
+          'fill-opacity',
+          layer.style.opacity,
+          MAPBOX_DEFAULTS['fill-opacity']
+        )
+      ]
+        .filter(Boolean)
+        .join(',\n');
     }
     case 'Choropleth': {
-      return intersperse(
-        [
-          `'fill-color': ${JSON.stringify(deriveColorScale(layer))}`,
-          withDefault(
-            'fill-opacity',
-            layer.style.opacity,
-            MAPBOX_DEFAULTS['fill-opacity']
-          )
-        ],
-        ',\n'
-      );
+      return [
+        `'fill-color': ${JSON.stringify(deriveColorScale(layer))}`,
+        withDefault(
+          'fill-opacity',
+          layer.style.opacity,
+          MAPBOX_DEFAULTS['fill-opacity']
+        )
+      ]
+        .filter(Boolean)
+        .join(',\n');
     }
     case 'Proportional Symbol': {
-      return intersperse(
-        [
-          withDefault(
-            'circle-color',
-            layer.style.fill,
-            MAPBOX_DEFAULTS['circle-color']
-          ),
-          `'circle-radius': ${JSON.stringify(deriveSize(layer))}`,
-          withDefault(
-            'circle-opacity',
-            layer.style.opacity,
-            MAPBOX_DEFAULTS['fill-opacity']
-          )
-        ],
-        ',\n'
-      );
+      return [
+        withDefault(
+          'circle-color',
+          layer.style.fill,
+          MAPBOX_DEFAULTS['circle-color']
+        ),
+        `'circle-radius': ${JSON.stringify(deriveSize(layer))}`,
+        withDefault(
+          'circle-opacity',
+          layer.style.opacity,
+          MAPBOX_DEFAULTS['fill-opacity']
+        )
+      ]
+        .filter(Boolean)
+        .join(',\n');
     }
     case 'Dot Density': {
-      return intersperse(
-        [
-          withDefault(
-            'circle-color',
-            layer.style.fill,
-            MAPBOX_DEFAULTS['circle-color']
-          ),
-          withDefault(
-            'circle-radius',
-            layer.style.dots.size,
-            MAPBOX_DEFAULTS['circle-radius']
-          ),
-          withDefault(
-            'circle-opacity',
-            layer.style.opacity,
-            MAPBOX_DEFAULTS['circle-opacity']
-          )
-        ],
-        ',\n'
-      );
+      return [
+        withDefault(
+          'circle-color',
+          layer.style.fill,
+          MAPBOX_DEFAULTS['circle-color']
+        ),
+        withDefault(
+          'circle-radius',
+          layer.style.dots.size,
+          MAPBOX_DEFAULTS['circle-radius']
+        ),
+        withDefault(
+          'circle-opacity',
+          layer.style.opacity,
+          MAPBOX_DEFAULTS['circle-opacity']
+        )
+      ]
+        .filter(Boolean)
+        .join(',\n');
     }
   }
 }
@@ -95,39 +91,37 @@ export function codegenStroke(layer: CartoKitLayer): string {
   switch (layer.type) {
     case 'Fill':
     case 'Choropleth': {
-      return intersperse(
-        [
-          withDefault(
-            'line-color',
-            layer.style.stroke,
-            MAPBOX_DEFAULTS['line-color']
-          ),
-          withDefault(
-            'line-width',
-            layer.style.strokeWidth,
-            MAPBOX_DEFAULTS['line-width']
-          )
-        ],
-        ',\n'
-      );
+      return [
+        withDefault(
+          'line-color',
+          layer.style.stroke,
+          MAPBOX_DEFAULTS['line-color']
+        ),
+        withDefault(
+          'line-width',
+          layer.style.strokeWidth,
+          MAPBOX_DEFAULTS['line-width']
+        )
+      ]
+        .filter(Boolean)
+        .join(',\n');
     }
     case 'Proportional Symbol':
     case 'Dot Density': {
-      return intersperse(
-        [
-          withDefault(
-            'circle-stroke-color',
-            layer.style.stroke,
-            MAPBOX_DEFAULTS['circle-stroke-color']
-          ),
-          withDefault(
-            'circle-stroke-width',
-            layer.style.strokeWidth,
-            MAPBOX_DEFAULTS['circle-stroke-width']
-          )
-        ],
-        ',\n'
-      );
+      return [
+        withDefault(
+          'circle-stroke-color',
+          layer.style.stroke,
+          MAPBOX_DEFAULTS['circle-stroke-color']
+        ),
+        withDefault(
+          'circle-stroke-width',
+          layer.style.strokeWidth,
+          MAPBOX_DEFAULTS['circle-stroke-width']
+        )
+      ]
+        .filter(Boolean)
+        .join(',\n');
     }
   }
 }
@@ -152,25 +146,4 @@ function withDefault<T extends string | number>(
         typeof cartokitValue === 'string' ? `'${cartokitValue}'` : cartokitValue
       }`
     : '';
-}
-
-/**
- * Interperse a set of values by a separator (without leaving a dangling
- * separator after the last value).
- *
- * @param values — The input array of values to separate.
- * @param sep – A string separator between values.
- * @returns – A string with values separated by @param sep.
- */
-function intersperse(values: string[], sep: string): string {
-  const v = values.filter(Boolean);
-
-  switch (v.length) {
-    case 0:
-      return '';
-    case 1:
-      return v[0];
-    default:
-      return v.slice(1).reduce((acc, el) => acc.concat(`${sep}${el}`), v[0]);
-  }
 }
