d886a8729b84efa0bcff95eb4ea4d53642fc8250
parkerziegler <parkerellisziegler@gmail.com>
2023-05-18 11:57:54 -0700

    fix: polish up MaptilerBasemapGrid

--- src/lib/components/basemap/MaptilerBasemapGrid.svelte
+++ src/lib/components/basemap/MaptilerBasemapGrid.svelte
@@ -1,54 +1,7 @@
-<!-- <script lang="ts">
-  import { onDestroy, onMount } from 'svelte';
-  import maplibregl from 'maplibre-gl';
-
-  import { PUBLIC_MAPTILER_API_KEY } from '$env/static/public';
-  import { ir } from '$lib/stores/ir';
-  import { MAPTILER_BASEMAPS, type Basemap } from '$lib/utils/basemap';
-
-  interface BasemapSelection extends Basemap {
-    map: maplibregl.Map;
-  }
-
-  let basemaps: BasemapSelection[] = [];
-
-  onMount(() => {
-    console.log($ir.center, $ir.zoom);
-
-    const bms = MAPTILER_BASEMAPS.map((basemap) => {
-      const map = new maplibregl.Map({
-        container: `tiles-${basemap.tileId}`,
-        style: `https://api.maptiler.com/maps/${basemap.tileId}/style.json?key=${PUBLIC_MAPTILER_API_KEY}`,
-        center: [15, 35],
-        zoom: 1
-      });
-
-      return { map, ...basemap };
-    });
-
-    basemaps = bms;
-  });
-
-  onDestroy(() => {
-    basemaps.forEach(({ map }) => {
-      map.remove();
-    });
-  });
-</script>
-
-<div class="grid grid-cols-3 gap-4">
-  {#each MAPTILER_BASEMAPS as basemap}
-    <div class="stack stack-sm">
-      <div id={`tiles-${basemap.tileId}`} class="h-32" />
-      <p>{basemap.title}</p>
-    </div>
-  {/each}
-</div> -->
 <script lang="ts">
   import { getContext } from 'svelte';
 
   import { PUBLIC_MAPTILER_API_KEY } from '$env/static/public';
-  import { addSource } from '$lib/interaction/source';
   import { ir } from '$lib/stores/ir';
   import { map } from '$lib/stores/map';
   import { MAPTILER_BASEMAPS, type Basemap } from '$lib/utils/basemap';
@@ -75,8 +28,15 @@
 
 <div class="grid grid-cols-3 gap-4">
   {#each MAPTILER_BASEMAPS as basemap}
-    <button class="flex flex-col" on:click={onSelectBasemap(basemap.tileId)}>
-      <img src={basemap.src} alt={`tiles-${basemap.tileId}`} class="h-32" />
+    <button
+      class="flex flex-col rounded-lg border border-transparent p-2 transition-colors hover:border-slate-400"
+      on:click={onSelectBasemap(basemap.tileId)}
+    >
+      <img
+        src={basemap.src}
+        alt={`tiles-${basemap.tileId}`}
+        class="h-32 w-auto rounded-xl"
+      />
       <p class="mt-2 text-sm font-semibold">{basemap.title}</p>
     </button>
   {/each}
