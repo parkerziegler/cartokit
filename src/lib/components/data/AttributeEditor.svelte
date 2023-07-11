1c1e92130130482f5a2e26758e24f40daa9071d5
parkerziegler <parkerellisziegler@gmail.com>
2023-07-11 16:31:56 -0700

    Begin work on #27. Add support for live preview to data transformation interface.

--- src/lib/components/data/AttributeEditor.svelte
+++ src/lib/components/data/AttributeEditor.svelte
@@ -3,6 +3,7 @@
   import { slide } from 'svelte/transition';
   import { basicSetup, EditorView } from 'codemirror';
   import { javascript } from '@codemirror/lang-javascript';
+  import { json } from '@codemirror/lang-json';
 
   import AlertIcon from '$lib/components/icons/AlertIcon.svelte';
   import CheckIcon from '$lib/components/icons/CheckIcon.svelte';
@@ -12,6 +13,7 @@
   import Menu from '$lib/components/shared/Menu.svelte';
   import MenuItem from '$lib/components/shared/MenuItem.svelte';
   import { dispatchLayerUpdate } from '$lib/interaction/update';
+  import { selectedFeature } from '$lib/stores/selected-feature';
   import type {
     CartoKitChoroplethLayer,
     CartoKitDotDensityLayer,
@@ -25,30 +27,57 @@
     | CartoKitProportionalSymbolLayer
     | CartoKitDotDensityLayer;
 
+  // Main editor state.
   let editor: HTMLDivElement;
   let view: EditorView;
   let error: string = '';
   let success: boolean = false;
+
+  // Preview editor state.
+  let editorPreview: HTMLDivElement;
+  let preview: EditorView;
+  let previewError: string = '';
+  let previewDoc: string = JSON.stringify(
+    {
+      type: $selectedFeature?.type,
+      properties: $selectedFeature?.properties,
+      geometry: $selectedFeature?.geometry
+    },
+    null,
+    2
+  );
   let timeoutId: number | undefined;
 
   export function focus() {
     view.focus();
   }
-
-  const doc = `function transformFeatures(features) {
-  return features;
-}`;
-
   onMount(() => {
     view = new EditorView({
-      doc,
-      extensions: [basicSetup, javascript()],
+      doc: `function transformFeatures(features) {
+  return features;
+}`,
+      extensions: [
+        basicSetup,
+        javascript(),
+        EditorView.updateListener.of((v) => {
+          if (v.docChanged) {
+            onChange();
+          }
+        })
+      ],
       parent: editor
+    });
+
+    preview = new EditorView({
+      doc: previewDoc,
+      extensions: [basicSetup, json(), EditorView.editable.of(false)],
+      parent: editorPreview
     });
   });
 
   function onClick() {
     const program = view.state.doc.toString();
+
     transformationWorker(program, layer.data.geoJSON.features, (message) => {
       if (message.type === 'data') {
         dispatchLayerUpdate({
@@ -74,13 +103,49 @@
     });
   }
 
+  function onChange() {
+    const program = view.state.doc.toString();
+
+    if ($selectedFeature) {
+      transformationWorker(program, [$selectedFeature], (message) => {
+        if (message.type === 'data' && message.data?.[0]) {
+          previewError = '';
+
+          previewDoc = JSON.stringify(
+            {
+              type: message.data[0].type,
+              properties: message.data[0].properties,
+              geometry: message.data[0].geometry
+            },
+            null,
+            2
+          );
+        } else if (message.type === 'data') {
+          previewError = '';
+
+          previewDoc = JSON.stringify(message.data?.[0], null, 2);
+        } else {
+          previewDoc = '';
+          previewError = message.error.message;
+        }
+      });
+    }
+  }
+
   onDestroy(() => {
     view.destroy();
+    preview.destroy();
 
     if (timeoutId) {
       window.clearTimeout(timeoutId);
     }
   });
+
+  $: if (preview) {
+    preview.dispatch({
+      changes: { from: 0, to: preview.state.doc.length, insert: previewDoc }
+    });
+  }
 </script>
 
 <Menu class="w-96">
@@ -90,7 +155,10 @@
       <p class="font-sans">
         Use the editor below to transform your dataset using JavaScript.
       </p>
-      <div bind:this={editor} class="-mx-4 overflow-auto bg-white text-black" />
+      <div
+        bind:this={editor}
+        class="transformation-editor -mx-4 overflow-auto bg-white text-black"
+      />
       {#if error}
         <div class="stack stack-xs" transition:slide>
           <div
@@ -123,5 +191,25 @@
         on:click={onClick}><span>Run</span><PlayCircle /></Button
       >
     </div>
+  </MenuItem>
+  <MenuItem title="Preview (1 selected feature)" titleClass="items-baseline">
+    <div
+      class="transformation-editor relative -mx-4 overflow-auto bg-white text-black"
+      bind:this={editorPreview}
+    />
+    <p slot="action" class="text-slate-400">OUTPUT</p>
+    {#if previewError}
+      <div
+        class="stack stack-xs rounded border border-red-400 bg-red-400 bg-opacity-50 px-2 py-1"
+      >
+        <div class="stack-h stack-h-xs items-center">
+          <AlertIcon />
+          <p class="font-sans">Failed to transform data. Original error:</p>
+        </div>
+        <p>
+          {previewError}
+        </p>
+      </div>
+    {/if}
   </MenuItem>
 </Menu>
--- src/lib/components/shared/MenuItem.svelte
+++ src/lib/components/shared/MenuItem.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import cs from 'classnames';
+
   export let title: string;
+  export let titleClass = '';
 </script>
 
 <div class="stack stack-xs p-4 font-mono text-xs text-white">
   {#if $$slots.action}
-    <div class="flex items-center justify-between">
+    <div class={cs('flex items-center justify-between', titleClass)}>
       <p class="font-sans text-base font-medium tracking-wider">
         {title}
       </p>
