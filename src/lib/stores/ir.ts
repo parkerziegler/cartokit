40cb0702957213c2b874f1759f64eee5c260d54d
parkerziegler <parkerellisziegler@gmail.com>
2023-09-23 13:25:23 -0500

    fix: Update basemap url in ir.ts.

--- src/lib/stores/ir.ts
+++ src/lib/stores/ir.ts
@@ -16,7 +16,7 @@ export interface CartoKitIR {
 export const ir = writable<CartoKitIR>({
   center: [-105, 37],
   basemap: {
-    url: "https://tiles.stadiamaps.com/styles/alidade_smooth.json",
+    url: 'https://tiles.stadiamaps.com/styles/alidade_smooth.json',
     provider: 'Stadia Maps'
   },
   zoom: 4,
