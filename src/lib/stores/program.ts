f1935c8325452db5fd3a6fee2c62f83a044607f4
parkerziegler <parkerellisziegler@gmail.com>
2023-09-23 19:02:12 -0700

    fix: Make codegen async to support prettier v3.

--- src/lib/stores/program.ts
+++ src/lib/stores/program.ts
@@ -1,7 +1,13 @@
-import { derived } from 'svelte/store';
+import type { Map } from 'maplibre-gl';
+import { derived, type Writable } from 'svelte/store';
 
 import { codegen } from '$lib/codegen';
-import { ir } from '$lib/stores/ir';
+import { ir, type CartoKitIR } from '$lib/stores/ir';
 import { map } from '$lib/stores/map';
 
-export const program = derived([map, ir], ([$map, $ir]) => codegen($map, $ir));
+export const program = derived<[Writable<Map>, Writable<CartoKitIR>], string>(
+  [map, ir],
+  ([$map, $ir], set) => {
+    codegen($map, $ir).then(set);
+  }
+);
