b7652fc5c0407e91d87b8e1d646939dbca1d640a
parkerziegler <parkerellisziegler@gmail.com>
2023-10-16 11:08:57 -0700

    fix: Move meta tags to +layout.svelte to work around issues with relative urls in SvelteKit.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,25 @@
 <script>
+  import ogImage from '$lib/assets/social/og-image.png';
   import '../app.css';
 </script>
 
 <svelte:head>
   <title>cartokit</title>
+  <meta
+    name="description"
+    content="cartokit is a new direct manipulation programming environment for interactive cartography on the Web."
+  />
+  <meta name="og:image" content={ogImage} />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:creator" content="@parker_ziegler" />
+  <meta name="twitter:image" content={ogImage} />
+  <meta
+    name="twitter:description"
+    content="cartokit is a new direct manipulation programming environment for interactive cartography on the Web."
+  />
   <link
     rel="stylesheet"
-    href="https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.css"
+    href="https://unpkg.com/maplibre-gl@3.3.1/dist/maplibre-gl.css"
   />
 </svelte:head>
 
