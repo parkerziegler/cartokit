e3326535c21d009854e988abf79f6c7dae9fc824
parkerziegler <parkerellisziegler@gmail.com>
2023-10-26 21:39:47 -0700

    fix: Attempt to fix og:image rendering.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,16 +7,21 @@
   <title>cartokit</title>
   <meta
     name="description"
-    content="cartokit is a new direct manipulation programming environment for interactive cartography on the Web."
+    content="cartokit is a new direct manipulation programming environment for interactive cartography on the web."
   />
   <meta name="og:image" content={ogImage} />
   <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@parker_ziegler" />
   <meta name="twitter:creator" content="@parker_ziegler" />
-  <meta name="twitter:image" content={ogImage} />
+  <meta
+    name="twitter:title"
+    content="cartokit: A direct manipulation programming environment for interactive cartography on the web."
+  />
   <meta
     name="twitter:description"
-    content="cartokit is a new direct manipulation programming environment for interactive cartography on the Web."
+    content="cartokit is a new direct manipulation programming environment for interactive cartography on the web."
   />
+  <meta name="twitter:image" content={ogImage} />
   <link
     rel="stylesheet"
     href="https://unpkg.com/maplibre-gl@3.3.1/dist/maplibre-gl.css"
