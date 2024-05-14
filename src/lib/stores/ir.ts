2c8522f53707b199d754519099863bc663f37a68
parkerziegler <parkerellisziegler@gmail.com>
2024-05-14 15:20:09 -0700

    fix: Use MapTiler as default basemap provider to avoid 401 errors on Vercel deployments.

--- src/lib/stores/ir.ts
+++ src/lib/stores/ir.ts
@@ -1,6 +1,8 @@
 import type { StyleSpecification } from 'maplibre-gl';
 import { writable } from 'svelte/store';
 
+// eslint-disable-next-line import/no-unresolved
+import { PUBLIC_MAPTILER_API_KEY } from '$env/static/public';
 import type { CartoKitLayer } from '$lib/types/CartoKitLayer';
 import type { BasemapProvider } from '$lib/utils/basemap';
 
@@ -18,8 +20,8 @@ export interface CartoKitIR {
 export const ir = writable<CartoKitIR>({
   center: [-98.35, 39.5],
   basemap: {
-    url: 'https://tiles.stadiamaps.com/styles/alidade_smooth.json',
-    provider: 'Stadia Maps'
+    url: `https://api.maptiler.com/maps/dataviz-light/style.json?key=${PUBLIC_MAPTILER_API_KEY}`,
+    provider: 'MapTiler'
   },
   zoom: 4,
   layers: {}
