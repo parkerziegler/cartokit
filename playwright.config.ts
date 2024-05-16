c4f4d68e29575e081d2fce8f79f76e87e5036d9e
parkerziegler <parkerellisziegler@gmail.com>
2024-05-15 23:00:39 -0700

    fix: Remove Firefox from Playwright config due to lack of WebGL support.

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,10 +41,6 @@ export default defineConfig({
       }
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
-    },
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] }
     }
