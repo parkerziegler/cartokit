12046d8e353e0de0796188722e5a1db972015854
parkerziegler <parkerellisziegler@gmail.com>
2024-07-12 10:23:26 -0700

    fix: lint

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,6 +1,7 @@
-import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
+// eslint-disable-next-line import/no-unresolved
+import { defineCollection } from 'astro:content';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({ schema: docsSchema() })
 };
