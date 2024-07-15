aad09dc20562488ef700095bc67a861919d95199
parkerziegler <parkerellisziegler@gmail.com>
2024-07-15 15:10:26 -0700

    feat: Move docs to separate repo.

--- docs/src/content/config.ts
+++
@@ -1,7 +0,0 @@
-import { docsSchema } from '@astrojs/starlight/schema';
-// eslint-disable-next-line import/no-unresolved
-import { defineCollection } from 'astro:content';
-
-export const collections = {
-  docs: defineCollection({ schema: docsSchema() })
-};
