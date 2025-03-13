43c873dfa0d63cbc8ca5bc6637a2e6a22b5e1e5e
Parker Ziegler <parkerellisziegler@gmail.com>
2025-03-13 11:34:59 -0700

    fix: Modify execute-workflows to use spawn in lieu of exec.

--- tests/scripts/execute-workflows.mjs
+++ tests/scripts/execute-workflows.mjs
@@ -1,18 +1,23 @@
-import { exec } from 'node:child_process';
+import { spawn } from 'node:child_process';
 
 function runCommand(command, times) {
-  if (times <= 0) return;
+  if (times <= 0) {
+    return;
+  }
   console.log(`Executing benchmarks ==> Trial: ${10 - times + 1}`);
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing command: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-    }
-    console.log(`stdout: ${stdout}`);
+  const playwright = spawn('pnpm', ['exec', 'playwright', 'test']);
+
+  playwright.stdout.on('data', (data) => {
+    console.log(data.toString().replaceAll('\n', ''));
+  });
+
+  playwright.stderr.on('data', (data) => {
+    console.error(data.toString().replaceAll('\n', ''));
+  });
+
+  playwright.on('close', (code) => {
+    console.log(`Child process exited with code ${code}.`);
     runCommand(command, times - 1);
   });
 }
