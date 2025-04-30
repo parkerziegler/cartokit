07b65966f7f2708e174350acc97147af78e61c27
Parker Ziegler <parkerellisziegler@gmail.com>
2025-04-29 21:30:38 -0700

    fix: Do not start a separate web server for Playwright in CI.

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,9 +44,11 @@ export default defineConfig({
     }
   ],
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173/',
-    reuseExistingServer: !process.env.CI
-  }
+  webServer: !process.env.CI
+    ? {
+        command: 'npm run dev',
+        url: 'http://localhost:5173/',
+        reuseExistingServer: true
+      }
+    : undefined
 });
