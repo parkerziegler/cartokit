62c92e1bcb339475dde81c9bc4a859571ee222f9
Parker Ziegler <parkerellisziegler@gmail.com>
2025-05-14 12:37:01 -0700

    fix: Add autofocus on initial render.

--- src/lib/components/shared/NumberInput.svelte
+++ src/lib/components/shared/NumberInput.svelte
@@ -7,6 +7,7 @@
     value: number;
     disabled?: boolean;
     class?: string;
+    autofocus?: boolean;
     onchange: (value: number) => void;
     ref?: HTMLInputElement;
   }
@@ -19,6 +20,7 @@
     value,
     disabled = false,
     class: className = '',
+    autofocus = false,
     onchange
   }: Props = $props();
 
@@ -35,6 +37,7 @@
   }
 </script>
 
+<!-- svelte-ignore a11y_autofocus -->
 <input
   type="number"
   {id}
@@ -43,6 +46,7 @@
   {value}
   {step}
   {disabled}
+  {autofocus}
   onchange={onChange}
   class={[
     'border border-transparent bg-inherit p-2 hover:border-slate-600 focus:border-slate-600',
